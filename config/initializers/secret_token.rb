52419e9f1a96d72db6b78f2b8092515d3038a0ea
nnaren1902 <nnaren1902@gmail.com>
2015-04-17 15:57:40 -0700

    empty app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VictoriousSecret::Application.config.secret_key_base = '20d6747dad453518e2465a3c74c782b581dfc928c09e490d007f9c43a05f439cc103709b72cca34d2316fa31deb43f373d6600e458c449fee424ff2aa8ec84ac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VictoriousSecret::Application.config.session_store :cookie_store, key: '_victorious-Secret_session'
