152db75910312706608ab0f1c59ff358c1a9a68d
Praveen Alevoor <praveenag91@gmail.com>
2015-04-24 17:24:53 -0700

    test cases and including devise in test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,9 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
+  include Devise::TestHelpers
+  include Warden::Test::Helpers
+  Warden.test_mode!
   ActiveRecord::Migration.check_pending!
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
