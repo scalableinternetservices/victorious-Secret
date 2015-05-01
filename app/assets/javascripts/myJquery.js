0a305217f3d2524cc61f6370d9b5e92d126f919a
avangari <akshay@s-169-232-241-158.resnet.ucla.edu>
2015-04-30 23:25:35 -0700

    navbar changes

--- app/assets/javascripts/myJquery.js
+++ app/assets/javascripts/myJquery.js
@@ -1,7 +1,4 @@
 	$(document).ready(function(){
-		if(window.location.pathname == "/users/sign_in"){
-			$(".header").hide();
-		} 
 		$( ".loginbtn" ).click(function() {
 		  $("#popup").modal('show');
 		});
