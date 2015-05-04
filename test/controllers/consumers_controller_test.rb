81698223248e362b5c13e0cc7411011931a3be47
Praveen Alevoor <praveenag91@gmail.com>
2015-05-04 09:06:39 -0700

    fixing the test errors - still 6 left

--- test/controllers/consumers_controller_test.rb
+++ test/controllers/consumers_controller_test.rb
@@ -6,17 +6,20 @@ class ConsumersControllerTest < ActionController::TestCase
   end
 
   test "should get index" do
+    sign_in users(:user1)
     get :index
     assert_response :success
     assert_not_nil assigns(:consumers)
   end
 
   test "should get new" do
+    sign_in users(:user1)
     get :new
     assert_response :success
   end
 
   test "should create consumer" do
+    sign_in users(:user1)
     assert_difference('Consumer.count') do
       post :create, consumer: { rating: @consumer.rating, user_id: @consumer.user_id }
     end
@@ -25,21 +28,25 @@ class ConsumersControllerTest < ActionController::TestCase
   end
 
   test "should show consumer" do
+    sign_in users(:user1)
     get :show, id: @consumer
     assert_response :success
   end
 
   test "should get edit" do
+    sign_in users(:user1)
     get :edit, id: @consumer
     assert_response :success
   end
 
   test "should update consumer" do
+    sign_in users(:user1)
     patch :update, id: @consumer, consumer: { rating: @consumer.rating, user_id: @consumer.user_id }
     assert_redirected_to consumer_path(assigns(:consumer))
   end
 
   test "should destroy consumer" do
+    sign_in users(:user1)
     assert_difference('Consumer.count', -1) do
       delete :destroy, id: @consumer
     end
