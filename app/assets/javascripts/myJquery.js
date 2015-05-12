9e0eeae5761dc810692abb6158442ff08a3ee2e4
avangari <akshayvangari@gmail.com>
2015-05-11 23:36:15 -0700

    I hate css...

--- app/assets/javascripts/myJquery.js
+++ app/assets/javascripts/myJquery.js
@@ -2,20 +2,20 @@
 		$( ".postmodal_link" ).click(function() {
 		  $("#popupthree").modal();
 		});
-		var vid = document.getElementById("wavegif");
-		vid.playbackRate = 3.0;
+
+		$("wavegif").playbackRate = 3.0;
+		
 		$('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
+		    e.preventDefault();
+		    var target = this.hash;
+		    var $target = $(target);
+		    $('html, body').stop().animate({
+		        'scrollTop': $target.offset().top
+		    }, 900, 'swing', function () {
+		        window.location.hash = target;
+		    });
+		});
 
-	    var target = this.hash;
-	    var $target = $(target);
-
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
 		$( ".loginbtn" ).click(function() {
 		  $("#popup").modal('show');
 		});
