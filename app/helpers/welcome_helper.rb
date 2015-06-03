d1b3f6776bc0ca7200748592290a154df2fa3a28
Praveen Alevoor <praveenag91@gmail.com>
2015-06-02 18:24:10 -0700

    Revert "Revert "caching for post tabs""
    This reverts commit 932a73e5c2a5a7ca52b23b694647aa6f8f5237e5.

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,8 @@
 module WelcomeHelper
+  def cache_key_for_post_row(post)
+    "post-#{post.id}-#{post.updated_at}"
+  end
+  def cache_key_for_post_user_row(post,id)
+    "post-#{post.id}-#{post.updated_at}-#{id}"
+  end
 end
